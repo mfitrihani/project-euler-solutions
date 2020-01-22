2f5cface636a5ca63374b5dd3a0dfc35d780a387
mfitrihani <mfitrihani@gmail.com>
2020-01-22 16:21:31 +0800

    solution 009

---
+++ Python/009.py
@@ -0,0 +1,10 @@
+def find_pythagorean_triplet_with_total_of(total_of_triplet):
+    for a in range(1, 999):
+        for b in range(a + 1, 999):
+            for c in range(b + 1, 999):
+                if c*c == a*a + b*b and a + b + c == total_of_triplet:
+                    return [a, b, c]
+
+
+if __name__ == '__main__':
+    print(find_pythagorean_triplet_with_total_of(1000))
