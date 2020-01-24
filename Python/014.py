f7a5fa9786281064f431f3622362682f83534412
mfitrihani <mfitrihani@gmail.com>
2020-01-24 11:24:18 +0800

    optimize solution 014 from 17s to 1.5s

--- Python/014.py
+++ Python/014.py
@@ -1,23 +1,27 @@
 import time
+
 start_time = time.time()
+sequence_count_table = {1: 1}
 
 
-def produce_collatz_sequence(number):
-    sequence = [number]
-    while number != 1:
-        if number % 2 == 0:
-            number = number / 2
-        else:
-            number = 3 * number + 1
-        sequence.append(number)
-    return sequence
+def calculate_collatz_sequence(number):
+    if number in sequence_count_table:
+        return sequence_count_table[number]
+    elif number % 2 == 0:
+        sequence_count = 1 + calculate_collatz_sequence(number / 2)
+        sequence_count_table[number] = sequence_count
+        return sequence_count
+    else:
+        sequence_count = 1 + calculate_collatz_sequence(3 * number + 1)
+        sequence_count_table[number] = sequence_count
+        return sequence_count
 
 
 def find_longest_collatz_sequence(maximum_starting_number):
     longest_sequence_number = 0
     max_sequence_length = 0
-    for x in range(2, maximum_starting_number+1):
-        sequence_length = len(produce_collatz_sequence(x))
+    for x in range(int(maximum_starting_number / 2), maximum_starting_number + 1):
+        sequence_length = calculate_collatz_sequence(x)
         if sequence_length > max_sequence_length:
             longest_sequence_number = x
             max_sequence_length = sequence_length
@@ -26,4 +30,4 @@ def find_longest_collatz_sequence(maximum_starting_number):
 
 if __name__ == '__main__':
     print(find_longest_collatz_sequence(1000000))
-    print(time.time()-start_time)
+    print(time.time() - start_time)
