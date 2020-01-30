934cae25264afbf9fa68aa029d0ae430019b72b5
mfitrihani <mfitrihani@gmail.com>
2020-01-30 08:40:37 +0800

    solution 041

--- Python/041.py
+++ Python/041.py
@@ -2,11 +2,26 @@ from itertools import permutations
 import time
 
 current_time = time.time()
-temp = list(permutations([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 10))
-palindrome_numbers = [int("".join([str(y) for y in x])) for x in temp]
+
+
+def produce_prime_palindrome_numbers():
+    temp = []
+    tem = []
+    for x in range(1, 10):
+        tem.append(x)
+        temp = temp + list(permutations(tem, x))
+    palindrome_numbers = [int("".join([str(y) for y in x])) for x in temp]
+    # filter for prime numbers
+    prime_numbers = []
+    for x in palindrome_numbers:
+        if is_prime(x):
+            prime_numbers.append(x)
+    return prime_numbers
 
 
 def is_prime(n):
+    if n <= 1:
+        return False
     if n == 2:
         return True
     else:
@@ -17,9 +32,5 @@ def is_prime(n):
 
 
 if __name__ == '__main__':
-    prime_numbers = []
-    for x in palindrome_numbers:
-        if is_prime(x):
-            prime_numbers.append(x)
-    print(prime_numbers)
+    print(produce_prime_palindrome_numbers())
     print(time.time() - current_time)
