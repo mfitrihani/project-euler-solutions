43ce292f6d8f03f4fd6f30486e1098a4aaad4292
mfitrihani <mfitrihani@gmail.com>
2020-02-04 12:24:03 +0800

    rewrite solution 003

--- Python/003.py
+++ Python/003.py
@@ -1,23 +1,33 @@
-def largest_prime_factor(n):
-    factors = find_factors(n)
-    prime_factors = []
-    for x in factors:
-        temp = find_factors(x)
-        if len(temp) == 0:
-            prime_factors.append(x)
-    return max(prime_factors)
+def is_prime(n):
+    if n <= 1:
+        return True
+    elif n <= 3:
+        return True
+    elif n % 2 == 0 or n % 3 == 0:
+        return False
+    else:
+        for x in range(5, int(n**0.5) + 1):
+            if n % x == 0:
+                return False
+        return True
 
 
 def find_factors(n):
-    # find factors
     factors = []
     for x in range(2, int(n ** 0.5) + 1):
         if n % x == 0:
-            factors.append(x)
-            factors.append(int(n / x))
+            if x == n/x:
+                factors.append(x)
+            else:
+                factors.append(x)
+                factors.append(n/x)
+    factors.sort()
+    factors.reverse()
     return factors
 
 
 if __name__ == '__main__':
-    print(largest_prime_factor(600851475143))
-    print(find_factors(12))
+    for x in find_factors(600851475143):
+        if is_prime(x):
+            print(x)
+            break
