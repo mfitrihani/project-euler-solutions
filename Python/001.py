31d98a1ac0905ddda85dd4e661cc61a9497bdb7b
mfitrihani <mfitrihani@gmail.com>
2020-02-04 11:47:15 +0800

    rewrite solution 001

--- Python/001.py
+++ Python/001.py
@@ -1,10 +1,3 @@
-def total_of_multiple_3and5(n):
-    total = 0
-    for x in range(0, n ):
-        if x % 3 == 0 or x % 5 == 0:
-            total = total + x
-    return total
-
-
 if __name__ == '__main__':
-    print(total_of_multiple_3and5(1000))
+    multiple_3or5 = [x for x in range(0, 1000) if x % 3 == 0 or x % 5 == 0]
+    print(sum(multiple_3or5))
