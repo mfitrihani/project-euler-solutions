e8bb2d623a8c7eeb93f35431ffecbfb5ef2ce8da
mfitrihani <mfitrihani@gmail.com>
2020-02-05 08:56:05 +0800

    solution 011

--- Python/011.py
+++ Python/011.py
@@ -1,66 +1,77 @@
-from copy import copy, deepcopy
-
-matrix = []
-matrix_temp = []
-
-
-def text_to_matrix(directory):
-    file = open(directory)
-    string = file.read()
-    char = [(s.split(" ")) for s in string.split("\n")]
-    global matrix
-    global matrix_temp
-    matrix = [[int(y) for y in x] for x in char]
-    matrix_temp = deepcopy(matrix)
-
-
-def find_greatest_adjacent_number(current_position):
-    global matrix
-    next_position = current_position.copy()
-    highest = -1
-    highest_position = []
-    for x in range(-1, 2):
-        next_position[0] = current_position[0] + x
-        for y in range(-1, 2):
-            next_position[1] = current_position[1] + y
-            if next_position != current_position and next_position[0] >= 0 and next_position[1] >= 0 and next_position[
-                0] < len(matrix) and \
-                    next_position[1] < len(matrix[0]):
-                if matrix[next_position[1]][next_position[0]] > highest:
-                    highest = matrix[next_position[1]][next_position[0]]
-                    highest_position = next_position.copy()
-    return highest_position
+digits = [
+    [8, 2, 22, 97, 38, 15, 00, 40, 00, 75, 4, 5, 7, 78, 52, 12, 50, 77, 91, 8],
+    [49, 49, 99, 40, 17, 81, 18, 57, 60, 87, 17, 40, 98, 43, 69, 48, 4, 56, 62, 00],
+    [81, 49, 31, 73, 55, 79, 14, 29, 93, 71, 40, 67, 53, 88, 30, 3, 49, 13, 36, 65],
+    [52, 70, 95, 23, 4, 60, 11, 42, 69, 24, 68, 56, 1, 32, 56, 71, 37, 2, 36, 91],
+    [22, 31, 16, 71, 51, 67, 63, 89, 41, 92, 36, 54, 22, 40, 40, 28, 66, 33, 13, 80],
+    [24, 47, 32, 60, 99, 3, 45, 2, 44, 75, 33, 53, 78, 36, 84, 20, 35, 17, 12, 50],
+    [32, 98, 81, 28, 64, 23, 67, 10, 26, 38, 40, 67, 59, 54, 70, 66, 18, 38, 64, 70],
+    [67, 26, 20, 68, 2, 62, 12, 20, 95, 63, 94, 39, 63, 8, 40, 91, 66, 49, 94, 21],
+    [24, 55, 58, 5, 66, 73, 99, 26, 97, 17, 78, 78, 96, 83, 14, 88, 34, 89, 63, 72],
+    [21, 36, 23, 9, 75, 00, 76, 44, 20, 45, 35, 14, 00, 61, 33, 97, 34, 31, 33, 95],
+    [78, 17, 53, 28, 22, 75, 31, 67, 15, 94, 3, 80, 4, 62, 16, 14, 9, 53, 56, 92],
+    [16, 39, 5, 42, 96, 35, 31, 47, 55, 58, 88, 24, 00, 17, 54, 24, 36, 29, 85, 57],
+    [86, 56, 00, 48, 35, 71, 89, 7, 5, 44, 44, 37, 44, 60, 21, 58, 51, 54, 17, 58],
+    [19, 80, 81, 68, 5, 94, 47, 69, 28, 73, 92, 13, 86, 52, 17, 77, 4, 89, 55, 40],
+    [4, 52, 8, 83, 97, 35, 99, 16, 7, 97, 57, 32, 16, 26, 26, 79, 33, 27, 98, 66],
+    [88, 36, 68, 87, 57, 62, 20, 72, 3, 46, 33, 67, 46, 55, 12, 32, 63, 93, 53, 69],
+    [4, 42, 16, 73, 38, 25, 39, 11, 24, 94, 72, 18, 8, 46, 29, 32, 40, 62, 76, 36],
+    [20, 69, 36, 41, 72, 30, 23, 88, 34, 62, 99, 69, 82, 67, 59, 85, 74, 4, 36, 16],
+    [20, 73, 35, 29, 78, 31, 90, 1, 74, 31, 49, 71, 48, 86, 81, 16, 23, 57, 5, 54],
+    [1, 70, 54, 71, 83, 51, 54, 69, 16, 92, 33, 48, 61, 43, 52, 1, 89, 19, 67, 48]
+]
 
 
-def find_consecutive_adjacent_number(consecutive, current_position):
-    positions = []
-    matrix[current_position[1]][current_position[0]] = 0
-    positions.append(current_position)
-    for x in range(0, consecutive - 1):
-        current_position = find_greatest_adjacent_number(current_position)
-        positions.append(current_position)
-        matrix[current_position[1]][current_position[0]] = 0
-    return positions
-
-
-def repeat_and_compare():
-    max_product = 1
-    product = 1
-    current_position = []
-    max_product_positions = []
-    for x in range(0, len(matrix[0])):
-        for y in range(0, len(matrix)):
-            current_position = find_consecutive_adjacent_number(4, [x, y])
-            for z in current_position:
-                product = product * matrix_temp[z[1]][z[0]]
-                print(current_position)
-            if product > max_product:
-                max_product = product
-                max_product_positions = current_position
-            product = 1
-    return max_product_positions
+def find_biggest_product():
+    biggest_product = 0
+    biggest_number = []
+    # horizontal
+    for x in range(0, len(digits)):
+        for y in range(0, len(digits[0]) - 3):
+            temp = digits[x][y]*digits[x][y+1]*digits[x][y+2]*digits[x][y+3]
+            if temp > biggest_product:
+                biggest_product = temp
+                biggest_number.clear()
+                biggest_number.append(digits[x][y])
+                biggest_number.append(digits[x][y+1])
+                biggest_number.append(digits[x][y+2])
+                biggest_number.append(digits[x][y+3])
+    # vertical
+    for x in range(0, len(digits) - 3):
+        for y in range(0, len(digits[0])):
+            temp = digits[x][y]*digits[x+1][y]*digits[x+2][y]*digits[x+3][y]
+            if temp > biggest_product:
+                biggest_product = temp
+                biggest_number.clear()
+                biggest_number.append(digits[x][y])
+                biggest_number.append(digits[x+1][y])
+                biggest_number.append(digits[x+2][y])
+                biggest_number.append(digits[x+3][y])
+    # diagonal slope
+    for x in range(0, len(digits)-3):
+        for y in range(0, len(digits[0])-3):
+            temp = digits[x][y]*digits[x+1][y+1]*digits[x+2][y+2]*digits[x+3][y+3]
+            if temp > biggest_product:
+                biggest_product = temp
+                biggest_number.clear()
+                biggest_number.append(digits[x][y])
+                biggest_number.append(digits[x+1][y+1])
+                biggest_number.append(digits[x+2][y+2])
+                biggest_number.append(digits[x+3][y+3])
+    # diagonal hill
+    for x in range(0, len(digits)):
+        for y in range(0, len(digits[0])-3):
+            temp = digits[x][y]*digits[x-1][y+1]*digits[x-2][y+2]*digits[x-3][y+3]
+            if temp > biggest_product:
+                biggest_product = temp
+                biggest_number.clear()
+                biggest_number.append(digits[x][y])
+                biggest_number.append(digits[x-1][y+1])
+                biggest_number.append(digits[x-2][y+2])
+                biggest_number.append(digits[x-3][y+3])
+    print(biggest_number)
+    print(biggest_product)
 
 
 if __name__ == '__main__':
-    text_to_matrix("C:\\Users\\MAMPU\\PycharmProjects\\project-euler-solutions\\Python\\011.txt")
-    print(repeat_and_compare())
+    print(find_biggest_product())
