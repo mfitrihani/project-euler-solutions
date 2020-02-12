736076ec92a40f2c849773c2ad2787c9d712a997
mfitrihani <mfitrihani@gmail.com>
2020-02-12 15:18:35 +0800

    solution 026

--- Python/026.py
+++ Python/026.py
@@ -1,44 +1,38 @@
 import decimal
+import itertools
+
+decimal.getcontext().prec = 10000
+finite = list(itertools.product([pow(5, x) for x in range(0, 5)], [pow(2, x) for x in range(0, 11)]))
+finite = [x[0]*x[1] for x in finite]
+finite = [x for x in finite if x <= 500]
 
 
 def find_recurring(denominator):
-    if is_power(denominator, 2) or is_power(denominator, 5) or denominator == 2 or denominator == 5:
-        return 0
-    elif is_prime(denominator):
-        test = decimal.Decimal(1/denominator)
-        print(test)
+    # return 0 for finite decimal point which is when the the denominator is 2^x*5^y
+    if denominator in finite:
+        return ""
+    else:
+        test = decimal.Decimal(decimal.Decimal(1) / decimal.Decimal(denominator))
         test = str(test)[2:]
-        index = 0
-        for x in range(1, len(test)):
-            if test[x] == test[0]:
-                index = x
-                break
-        return test[:index]
-
-
-def is_power(x, y):
-    if y == 1:
-        return x == 1
-    elif x == 1:
-        return y == 1
-    else:
-        pow = 1
-        while pow < x:
-            pow *= y
-        return pow == x
-
-
-def is_prime(n):
-    if n <= 1:
-        return False
-    elif n == 2:
-        return True
-    else:
-        for x in range(2, int(n**0.5) + 1):
-            if n % x == 0:
-                return False
-        return True
+        start, end = 0, 1
+        compare = test[start:end]
+        while compare != test[end:end + len(compare)] or compare != test[end + len(compare): end + len(compare)*2] or compare != test[end + 2*len(compare): end + len(compare)*3]:
+            if end == len(test):
+                start += 1
+                end = start + 1
+            else:
+                end += 1
+            compare = test[start:end]
+        return compare
 
 
 if __name__ == '__main__':
-    print(find_recurring(17))
+    max_length = 0
+    max_number = 0
+    for x in range(2, 1000):
+        temp = len(find_recurring(x))
+        if temp > max_length:
+            max_number = x
+            max_length = temp
+    print(max_number)
+    print(max_length)
